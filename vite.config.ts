49e7473f5bc414ff7eccf12bec948cd152e7c524
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-14 10:49:04 -0300

    feat(vite.config.ts): configure server settings for external access
    - Enable external access by setting `host` to true.
    - Specify local port as 5173 with `strictPort` enabled.
    - Allow ngrok domains in `allowedHosts` for easier testing.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // allows external access
+    port: 5173, // your local port
+    strictPort: true,
+    // allow your ngrok domain
+    allowedHosts: ['.ngrok-free.app', '.ngrok.io'],
+  },
+});
