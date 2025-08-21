ce71d2a12d14b40a931b63c7ba0df131eb912198
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-08-21 10:21:28 -0300

    feat(NotFoundPage): add localized 404 page with styling
    - Introduced a new `notFoundPage.json` for English and Portuguese translations.
    - Created `NotFoundPage.module.css` for styling the 404 page layout.
    - Updated `NotFoundPage.tsx` to utilize translations and styles for a better user experience.

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,4 +1,20 @@
+import { MdOutlineReportGmailerrorred } from 'react-icons/md';
+import { useTranslation } from 'react-i18next';
+import Button from '../../components/Button/Button.tsx';
+import styles from './NotFoundPage.module.css';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const { t } = useTranslation('notFoundPage');
+  return (
+    <div className={styles.pageContent}>
+      <h2 className={styles.h2}>
+        <MdOutlineReportGmailerrorred size={40} /> {t('title')}
+      </h2>
+      <p>{t('description')}</p>
+      <Button to="/" disabled={false}>
+        {t('button')}
+      </Button>
+    </div>
+  );
 };
 export default NotFoundPage;
