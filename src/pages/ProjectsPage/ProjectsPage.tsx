0b54ce83f41cb347449bc97def73a2023209a5a2
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2026-02-12 12:05:59 -0300

    feat: update projects descriptions

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -17,44 +17,45 @@ const ProjectsPage: React.FC = () => {
     ];
 
     const reactSwiftStacksPoints = [
-        t('reactSwiftStacks.points.point1'),
-        t('reactSwiftStacks.points.point2'),
-        t('reactSwiftStacks.points.point3'),
-        t('reactSwiftStacks.points.point4'),
+        t('projectsPage:reactSwiftStacks.points.point1'),
+        t('projectsPage:reactSwiftStacks.points.point2'),
+        t('projectsPage:reactSwiftStacks.points.point3'),
+        t('projectsPage:reactSwiftStacks.points.point4'),
     ];
     const miniCommunityPoints = [
-        t('miniCommunity.points.point1'),
-        t('miniCommunity.points.point4'),
-        t('miniCommunity.points.point3'),
-        t('miniCommunity.points.point2'),
+        t('projectsPage:miniCommunity.points.point1'),
+        t('projectsPage:miniCommunity.points.point2'),
+        t('projectsPage:miniCommunity.points.point3'),
+        t('projectsPage:miniCommunity.points.point4'),
     ];
     const gameJournalPoints = [
-        t('gameJournal.points.point1'),
-        t('gameJournal.points.point2'),
-        t('gameJournal.points.point3'),
-        t('gameJournal.points.point4'),
+        t('projectsPage:gameJournal.points.point1'),
+        t('projectsPage:gameJournal.points.point2'),
+        t('projectsPage:gameJournal.points.point3'),
+        t('projectsPage:gameJournal.points.point4'),
     ];
     const mernTaskManagerPoints = [
-        t('mernTaskManager.points.point1'),
-        t('mernTaskManager.points.point2'),
-        t('mernTaskManager.points.point3'),
-        t('mernTaskManager.points.point4'),
+        t('projectsPage:mernTaskManager.points.point1'),
+        t('projectsPage:mernTaskManager.points.point2'),
+        t('projectsPage:mernTaskManager.points.point3'),
+        t('projectsPage:mernTaskManager.points.point4'),
     ];
     const weatherTailWindPoints = [
-        t('weatherTailWind.points.point1'),
-        t('weatherTailWind.points.point2'),
-        t('weatherTailWind.points.point3'),
+        t('projectsPage:weatherTailWind.points.point1'),
+        t('projectsPage:weatherTailWind.points.point2'),
+        t('projectsPage:weatherTailWind.points.point3'),
+        t('projectsPage:weatherTailWind.points.point4'),
     ];
     const boxShadowGeneratorPoints = [
-        t('boxShadowGenerator.points.point1'),
-        t('boxShadowGenerator.points.point2'),
-        t('boxShadowGenerator.points.point3'),
+        t('projectsPage:boxShadowGenerator.points.point1'),
+        t('projectsPage:boxShadowGenerator.points.point2'),
+        t('projectsPage:boxShadowGenerator.points.point3'),
     ];
     const stickiesPoints = [
-        t('stickies.points.point1'),
-        t('stickies.points.point2'),
-        t('stickies.points.point3'),
-        t('stickies.points.point4'),
+        t('projectsPage:stickies.points.point1'),
+        t('projectsPage:stickies.points.point2'),
+        t('projectsPage:stickies.points.point3'),
+        t('projectsPage:stickies.points.point4'),
     ];
 
     return (
@@ -68,28 +69,28 @@ const ProjectsPage: React.FC = () => {
                 />
 
                 <ProjectCard
-                    title={t('reactSwiftStacks.title')}
+                    title={t('projectsPage:reactSwiftStacks.title')}
                     points={reactSwiftStacksPoints}
                     githubUrl="https://github.com/ednanf/react-swiftstacks"
                     demoUrl="https://www.npmjs.com/package/react-swiftstacks"
                 />
 
                 <ProjectCard
-                    title={t('miniCommunity.title')}
+                    title={t('projectsPage:miniCommunity.title')}
                     points={miniCommunityPoints}
                     githubUrl="https://github.com/ednanf/mini-community"
                     demoUrl="https://mini-community.frizzera.dev"
                 />
 
                 <ProjectCard
-                    title={t('gameJournal.title')}
+                    title={t('projectsPage:gameJournal.title')}
                     points={gameJournalPoints}
                     githubUrl="https://github.com/ednanf/game-journal"
                     demoUrl="https://game-journal.frizzera.dev "
                 />
 
                 <ProjectCard
-                    title={t('mernTaskManager.title')}
+                    title={t('projectsPage:mernTaskManager.title')}
                     // Resize in portuguese due to length
                     {...(isPt ? { titleKeyFontSize: 20 } : {})}
                     points={mernTaskManagerPoints}
@@ -98,21 +99,21 @@ const ProjectsPage: React.FC = () => {
                 />
 
                 <ProjectCard
-                    title={t('weatherTailWind.title')}
+                    title={t('projectsPage:weatherTailWind.title')}
                     points={weatherTailWindPoints}
                     githubUrl="https://github.com/ednanf/weather-tailwind"
                     demoUrl="https://tailwind.frizzera.dev"
                 />
 
                 <ProjectCard
-                    title={t('boxShadowGenerator.title')}
+                    title={t('projectsPage:boxShadowGenerator.title')}
                     points={boxShadowGeneratorPoints}
                     githubUrl="https://github.com/ednanf/box-shadow-generator"
                     demoUrl="https://ednanf.github.io/box-shadow-generator"
                 />
 
                 <ProjectCard
-                    title={t('stickies.title')}
+                    title={t('projectsPage:stickies.title')}
                     points={stickiesPoints}
                     githubUrl="https://github.com/ednanf/stickies"
                     demoUrl="https://ednanf.github.io/stickies"
